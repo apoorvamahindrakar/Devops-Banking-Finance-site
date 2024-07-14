2376aeace4d7b5260031a630eead92672874cf24
apoorvamahindrakar <111277926+apoorvamahindrakar@users.noreply.github.com>
2024-07-14 15:38:14 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -9,19 +9,19 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1b"
+  region = "ap-south-1"
 }
 
 # Create an EC2 instance
-resource "aws_instance" "Test-Server" {
+resource "aws_instance" "Prod-Server" {
   ami               = "ami-0ad21ae1d0696ad58"  # Ubuntu AMI ID for us-east-1
   instance_type     = "t2.micro"
-  availability_zone = "ap-south-1b"
+  availability_zone = "ap-south-1"
   key_name          = "Banking-app"  # Replace with your actual key pair name
 
   vpc_security_group_ids = ["sg-09b21d30c0eea4629"]  # Security Group ID
 
   tags = {
-    Name = "Test-Server"
+    Name = "Prod-Server"
   }
 }
