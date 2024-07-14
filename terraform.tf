937f707754eaef4cc3d64c6de97e95cc27005892
apoorvamahindrakar <111277926+apoorvamahindrakar@users.noreply.github.com>
2024-07-14 15:55:15 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -125,11 +125,11 @@ resource "aws_eip" "proj-eip" {
 
 
 # Creating an ubuntu EC2 instance
-resource "aws_instance" "Prod-Server" {
+resource "aws_instance" "Test-Server" {
  ami = "ami-0ef82eeba2c7a0eeb"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1b"
- key_name = "banking-app"
+ key_name = "Banking-app"
  network_interface {
  device_index = 0
  network_interface_id = aws_network_interface.proj-ni.id
@@ -139,6 +139,6 @@ resource "aws_instance" "Prod-Server" {
      sudo apt-get update -y
  EOF
  tags = {
- Name = "Prod-Server"
+ Name = "Test-Server"
  }
 }
